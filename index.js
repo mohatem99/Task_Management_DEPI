c18e02f08b71338fd1a9a8bbd4cd175f215e1501
Mohamed Hatem <68520226+mohatem99@users.noreply.github.com>
2024-10-01 13:09:20 +0300

    adding cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from "cors";
 import { config } from "dotenv";
 import dbConnection from "./src/db/dbconnection.js";
 
@@ -11,7 +11,7 @@ config();
 
 dbConnection();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
